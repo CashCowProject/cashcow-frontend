f418c634d05ffecd63c3c55235fb1573396ec0ea
CashCow <>
2021-10-17 20:35:07 +0200

    Added more decimal places for the milk price

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,13 +5,17 @@ import { LanguageContext } from 'contexts/Localisation/languageContext'
 import useTheme from 'hooks/useTheme'
 import { usePriceCakeBusd } from 'state/hooks'
 import { Menu as UikitMenu } from '@pancakeswap-libs/uikit'
+
 import config from './config'
+
 
 const Menu = (props) => {
   const { account, connect, reset } = useWallet()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
   const cakePriceUsd = usePriceCakeBusd()
+
+  // console.log('cakePriceUsd', new bigNumber(cakePriceUsd))
 
   return (
     <UikitMenu
