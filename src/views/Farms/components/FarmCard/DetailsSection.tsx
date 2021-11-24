e97ca4f3446c27c1ecdecf1fd6c3aa958d11d3d5
CashCow <>
2021-11-24 21:03:34 +0100

    Created a fix for buying at gol

--- src/views/Farms/components/FarmCard/DetailsSection.tsx
+++ src/views/Farms/components/FarmCard/DetailsSection.tsx
@@ -55,8 +55,8 @@ const DetailsSection: React.FC<ExpandableSectionProps> = ({
         <StyledLinkExternal
           href={
             isTokenOnly
-              ? `https://pancakeswap.finance/swap?outputCurrency=${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
-              : `https://pancakeswap.finance/add/${liquidityUrlPathParts}`
+              ? `${lpLabel === 'GOL' ? 'https://gol.finance' : 'https://pancakeswap.finance'}/swap?outputCurrency=${tokenAddresses[process.env.REACT_APP_CHAIN_ID]}`
+              : `${lpLabel === 'GOL' ? 'https://gol.finance' : 'https://pancakeswap.finance'}/add/${liquidityUrlPathParts}`
           }
         >
           {lpLabel}
