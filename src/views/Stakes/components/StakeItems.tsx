d5446e096587b8e1487f5b218e05fe5e7a1bdcbb
MiracleCorunus <miraclegwon@gmail.com>
2022-03-08 05:39:59 +0900

    fix responsieve issue on staking item

--- src/views/Stakes/components/StakeItems.tsx
+++ src/views/Stakes/components/StakeItems.tsx
@@ -4,23 +4,29 @@ import { StakeContext } from 'contexts/StakeContext'
 import { StakeItem, NewItem } from './ItemComponents'
 
 const StakeItemContainer = styled.div`
-    display: flex;
-    width: 100%;
-    flex-wrap: wrap;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
 `
-const StakeItems = ({index}) => {
+
+const StakeItemEach = styled.div`
+  width: 25%;
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+`
+const StakeItems = ({ index }) => {
   const { selectedNFTS } = useContext(StakeContext)
   return (
     <StakeItemContainer>
-        {selectedNFTS.map((itm) => (
-          <div style={{width: '25%'}}>
-            <StakeItem key={itm.tokenId} data={itm} index={index}/>
-          </div>
-        ))}
-        <div style={{width: '25%'}}>
-          <NewItem index={index}/>
-        </div>
-
+      {selectedNFTS.map((itm) => (
+        <StakeItemEach>
+          <StakeItem key={itm.tokenId} data={itm} index={index} />
+        </StakeItemEach>
+      ))}
+      <StakeItemEach>
+        <NewItem index={index} />
+      </StakeItemEach>
     </StakeItemContainer>
   )
 }
