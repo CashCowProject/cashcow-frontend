13709ecb5d138aea494c023dbd9e09457a268277
MiracleCorunus <miraclegwon@gmail.com>
2022-03-08 05:31:21 +0900

    update issue on price of reward milk

--- src/views/Stakes/components/StaticComponents/Harvest.tsx
+++ src/views/Stakes/components/StaticComponents/Harvest.tsx
@@ -125,7 +125,7 @@ const Harvet = ({ rewardAllMilk, index }) => {
           {getNumberSuffix(rewardAllMilk / 1000000, 3)}
         </Text>
         <Text textTransform="uppercase" color="textSubtle" fontSize="18px" style={{ lineHeight: 2 }}>
-          {`≈ $${cakePriceUsd.toNumber() * rewardAllMilk}`}
+          {`≈ $${(cakePriceUsd.toNumber() * rewardAllMilk) / 1000000}`}
         </Text>
       </Flex>
     </Flex>
