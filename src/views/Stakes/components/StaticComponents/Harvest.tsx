36d5e5df4fbf6458c6fb2c5c2b69b60e90fe3e88
MiracleCorunus <miraclegwon@gmail.com>
2022-03-10 06:02:48 +0900

    fix issues on style

--- src/views/Stakes/components/StaticComponents/Harvest.tsx
+++ src/views/Stakes/components/StaticComponents/Harvest.tsx
@@ -125,7 +125,7 @@ const Harvet = ({ rewardAllMilk, index }) => {
           {getNumberSuffix(rewardAllMilk / 1000000, 3)}
         </Text>
         <Text textTransform="uppercase" color="textSubtle" fontSize="18px" style={{ lineHeight: 2 }}>
-          {`≈ $${(cakePriceUsd.toNumber() * rewardAllMilk) / 1000000}`}
+          {`≈ $${getNumberSuffix((cakePriceUsd.toNumber() * rewardAllMilk) / 1000000, 7)}`}
         </Text>
       </Flex>
     </Flex>
